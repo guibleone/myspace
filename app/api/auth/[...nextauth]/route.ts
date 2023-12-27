8d4a64606b1c6fcc25082ca54fbfc1cf7f28858f
Guilherme Bastos Leone <guilhermebastosleone@gmail.com>
2023-12-27 15:59:09 -0300

    Update .gitignore and route.ts files

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,6 @@
-import { prisma } from "@/lib/prisma";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth from "next-auth";
-import type { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-
-export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!
-        })
-    ],
-    session: {
-        strategy: "jwt",
-    },
-}
+import { authOptions } from "@/utils/authOptions";
+import NextAuth from "next-auth/next";
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST }
+
+export { handler as GET, handler as POST };
